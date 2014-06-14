b4b6d4156de3795583c440c79fde9032270ff872
Brian Iversen <biversen21@gmail.com>
2014-06-14 11:13:32 -0700

    Started index templates

--- js/index.js
+++ js/index.js
@@ -1,8 +1,10 @@
 $(function() {
 
 var Router = Backbone.Router.extend ({
-	'': 'home',
-	'new': 'editProfile'
+	routes: {
+		'': 'home',
+		'new': 'editProfile'
+	}
 }); 
 
 var UserProfile = Backbone.Model.extend ({
@@ -13,20 +15,38 @@ var UserList = Backbone.Collection.extend ({
 	localStorage: new Backbone.LocalStorage('users')
 });
 
-var NewUserView = Backbone.View.extend ({
-	render: function({
-	
-	})
+var HomeView = Backbone.View.extend ({
+	el: 'page',
+	render: function(){
+		var template = _.template($('#sign-in-template').html());//, {userProfile: userProfile.models});
+		this.$el.html('<h3>Test</h3>');
+		console.log(template);
+	}
+})
+
+var UserView = Backbone.View.extend ({
+	render: function(){
+		var that = this; 
+		userProfile = new UserProfile();
+		userProfile.fetch({
+			success: function(){
+				console.log('Test');
+			}
+		})
+	}
 });
 
+var homeView = new HomeView();
+var userView = new UserView();
 var router = new Router;
 	
 router.on('route:home', function(){
-	
-};
+	homeView.render();
+	console.log('test');
+});
 router.on('route:editProfile', function() {
-	
-};
+	userView.render();
+});
 
 Backbone.history.start();
-}
+});
